2dd3fa0107d772c41ff565d561c6065ae971d4fe
Michael Trommer <mictro@gmail.com>
2009-09-08 02:46:58 +0200

    basic history stuff

--- app/public/js/ams.js
+++ app/public/js/ams.js
@@ -1,19 +1,26 @@
 
 // history
 function mk_history_link() {
-    var target = ($(this).attr("href"));
-    //return false;
 
+    var target = $(this);
+    alert(target);
     $.ajax({
-        url: target,
+        url: target.attr("href"),
         success: function(result) {
             $("#content").html(result);
+            history_append(target);
+            return false;
         }
     });
     return false;
 
 };
-//}
+function history_append(ele){
+    var target = $('#history_inc');
+    var html = '<li class="column"><a return false;" class="alink" href="' + ele.attr("href") +'" title="'+ ele.attr("title") +'">' + ele.attr("title") +'</a></li>';
+    target.append(html);
+    $(target).find("a:last").bind("click", mk_history_link);
+}
 
 
 
