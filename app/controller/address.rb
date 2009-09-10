87d4faa09115d6630a83094883e068bad95c1ca7
Michael Trommer <mictro@gmail.com>
2009-09-10 05:37:04 +0200

    address controller

---
+++ app/controller/address.rb
@@ -0,0 +1,32 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+class AddressController < AMSController
+  map "/address"
+
+  def index
+    "add"
+  end
+
+  def sidebar
+    "add sb"
+  end
+
+  def address_for(uid_or_email)
+    @id = uid_or_email # needed in template
+  end
+  
+end
+
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
