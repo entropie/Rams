dac95f413ac86bb6305bc2a0bc3d0f073d4409a6
Michael Trommer <mictro@gmail.com>
2009-09-17 01:18:02 +0200

    Time#ago and #to_s

--- lib/ruby/Time.rb
+++ lib/ruby/Time.rb
@@ -12,6 +12,36 @@ end
 
 class Time
   
+  # stolen and adapted from rails / ramaze ;)
+  def ago(to_time = Time.now, include_seconds = false)
+    from_time = self
+    distance_in_minutes = (((to_time - from_time).abs)/60).round
+    distance_in_seconds = ((to_time - from_time).abs).round if include_seconds
+
+    case distance_in_minutes
+    when 0..1
+      return (distance_in_minutes == 0) ? 'less than a minute' : '1 minute' unless include_seconds
+      case distance_in_seconds
+      when 0..4   then 'weniger als 5 sekunden'
+      when 5..9   then 'weniger als 10 sekunden'
+      when 10..19 then 'weniger als 20 sekunden'
+      when 20..39 then 'einer halben minute'
+      when 40..59 then 'weniger als einer minute'
+      else             '1 minute'
+      end
+
+    when 2..44           then "#{distance_in_minutes} minuten"
+    when 45..89          then 'einer stunde'
+    when 90..1439        then "#{(distance_in_minutes.to_f / 60.0).round} stunden"
+    when 1440..2879      then 'ein tag'
+    when 2880..43199     then "#{(distance_in_minutes / 1440).round} tagen"
+    when 43200..86399    then 'einem monat'
+    when 86400..525959   then "#{(distance_in_minutes / 43200).round} monaten"
+    when 525960..1051919 then 'einem jahr'
+    else                      "über #{(distance_in_minutes / 525960).round} jahren"
+    end
+  end
+  
   def iso_cweek
     thursday = self.beginning_of_week + 3.days
     (thursday.yday - (Time.mktime(thursday.year, 1, 4).beginning_of_week + 3.days).yday)/ 7 + 1
@@ -34,8 +64,12 @@ class Time
     strftime("%Y") + iso_cweek.to_s
   end
 
-  def to_s
-    strftime("%d %b. %y")
+  def to_s(wago = true)
+    if wago
+      ago(Time.now, true)
+    else
+      strftime("%d %b. %y")
+    end
   end
   
   def nts
