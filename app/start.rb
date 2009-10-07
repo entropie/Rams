fef2883ccb09a58f40e1118b87f1e53770b2e9ba
Michael Trommer <mictro@gmail.com>
2009-10-08 00:18:10 +0200

    cleanup and different host/port on remote machine

--- app/start.rb
+++ app/start.rb
@@ -33,52 +33,15 @@ controller = %w"ams page css user address messages auth location products todo a
 libs = []
 
 class Innate::Session
-
   public :cookie
 end
 (controller + libs).each{|lib| require lib}
 
-# p AMSController.controller
-#p AMSController.controller_at "/"
-# p AMSController.controller_at "/"
 
-Ramaze.start(:host => "10.0.187.12",
-             :port => 8080,
-             :adapter => :mongrel
-              )
 
-# require 'controller/css'
-# require 'controller/paste'
-# require 'controller/spam'
-# require 'controller/account'
- 
-# class Highlight
-#   def default_options
-#     { :engine => $rapaste[:engine],
-#       :fragment => $rapaste[:fragment] }
-#   end
- 
-#   mod = EXTEND[$rapaste[:engine]]
-#   mod.setup(__DIR__)
-#   $rapaste_syntaxes = mod.syntaxes($rapaste[:priority])
-# end
- 
-# # The Bayes database contains information about the ham and spam rating of
-# # certain words.
-# # If you would like to reset it, just remove the db/bayes.marshal file.
-# BAYES = Bayes.new( File.join( __DIR__, 'db/bayes.marshal' ) )
- 
-# # Initial seeding of the bayes filter, setting up categories and a couple of
-# # common ratings.
-# # The format of the files isn't that important, given that it should recognize
-# # any text.
-# # But you should separate words in some way (whitespace, commas, numbers...)
-# if BAYES.categories.empty?
-#   BAYES.train(:spam, File.read( File.join( __DIR__, 'db/spam.txt' ) ))
-#   BAYES.train(:ham, File.read( File.join( __DIR__, 'db/ham.txt' ) ))
-# end
- 
-# Ramaze.start($rapaste[:ramaze]) if $0 == __FILE__
+Ramaze.start(:host => Rams::PLATFORM == :darwin ? "10.0.187.12" : "grakunst.de",
+             :port => Rams::PLATFORM == :darwin ? 8080 : 23000,
+             :adapter => :mongrel)
 
 
 =begin
