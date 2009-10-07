299ee7b6bd8404c1475a1681ec22bad27cc2da59
Michael Trommer <mictro@gmail.com>
2009-10-08 01:00:48 +0200

    cap tasks moved to own dir in script/

---
+++ script/capistrano/tasks.rb
@@ -0,0 +1,30 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+require 'net/sftp'
+require "lib/ams"
+
+ramaze_hash = "5dda528952206cb2b5943d65ab7427e692482881"
+task :get_ramaze do
+  run "cd /u/apps && rm -rf ramaze"
+  run "cd /u/apps/ && git clone git://github.com/manveru/ramaze.git; cd ramaze && git checkout -b deploy #{ramaze_hash}"
+end
+
+
+after "deploy:symlink" do
+  tf = "#{current_path}/.mysql.pw"
+  run "touch  #{tf}"
+  put(File.readlines(File.join(Rams::Source, ".mysql.pw")).join, tf)
+end
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
